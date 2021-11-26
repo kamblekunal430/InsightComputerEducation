45f62eee050af1918165282cc8fbf0ed34224a21
kamblekunal430 <kamblekunal430@gmail.com>
2021-11-26 12:51:56 +0530

    added mscit course details link to products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,23 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  msg: any;
   constructor() {}
 
   ngOnInit(): void {}
 
-  clickme() {
-    this.msg = `<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Toggle bottom offcanvas</button>
-
-<div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasBottomLabel">Offcanvas bottom</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body small">
-    ...
-  </div>
-</div>`;
-    return this.msg;
-  }
+  links = [
+    'https://mscit.mkcl.org/course-details',
+    'https://klic.mkcl.org/klic-courses/advanced-tally',
+    'https://klic.mkcl.org/klic-courses/tally-gst',
+    'http://mastering.mkcl.org/typing/',
+    'https://klic.mkcl.org/klic-diploma#',
+    'http://english.mkcl.org/',
+    'https://klic.mkcl.org/',
+    'https://klic.mkcl.org/klic-courses/c-plusplus',
+    'https://klic.mkcl.org/klic-courses/c-programming',
+  ];
 }
